115e99c84024e079f0e542bb2116bb2c0ab48c6b
xxybzds <1208847284@qq.com>
2023-05-20 14:54:55 +0800

    初始布局版本

---
+++ src/components/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="index">
+    <nav>
+      <van-tabbar route v-model="active">
+        <van-tabbar-item replace to="/home" class="iconfont icon-faxian"
+          >发现</van-tabbar-item
+        >
+        <van-tabbar-item replace to="/search" class="iconfont icon-boke1-copy"
+          >播客</van-tabbar-item
+        >
+        <van-tabbar-item
+          replace
+          to="/search"
+          class="iconfont icon-gedan"
+          active-color="skyblue"
+          >歌单</van-tabbar-item
+        >
+      </van-tabbar>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
+</script>
+<style lang='less' scoped>
+.iconfont {
+  font-size: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import "@/assets/iconfont/iconfont.css"
+import "@/assets/reset.css"
+Vue.use(Vant);
+import { Tabbar, TabbarItem } from 'vant';
+Vue.use(Tabbar).use(TabbarItem);
 Vue.config.productionTip = false
 
 new Vue({
